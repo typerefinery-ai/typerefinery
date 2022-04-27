915748fefee7e8fd298b9dc4f429965245f4bb31
wildone <max.barrass@gmail.com>
2022-04-28 00:05:06 +1000

    add query api.

--- services/fastapi/main.py
+++ services/fastapi/main.py
@@ -1,6 +1,9 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+import json
+from scripts.G_to_WebCola import get_data as gquery
+
 
 app = FastAPI()
 
@@ -13,3 +16,14 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/query")
+async def read_query(url: str = Form(...), port: str = Form(...), database: str = Form(...), gQuery: str = Form(...)):
+    gConnect = {}
+    gConnect['url'] = url
+    gConnect['port'] = port
+    gConnect['database'] = database
+    gConnect['gQuery'] = gQuery
+    # query grakn
+    colaGraph = gquery(gConnect=gConnect)
+    return json.dumps(colaGraph)
