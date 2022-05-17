d0aece19a9284e895f01fcdbfb3a91fe05b8d97b
wildone <max.barrass@gmail.com>
2022-05-18 00:13:16 +1000

    fix localstorage name.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import { createStore } from "vuex"
 import VuexPersistence from "vuex-persist"
 
 const vuexLocal = new VuexPersistence({
+  key: "typerefinery",
   storage: window.localStorage,
 })
 
