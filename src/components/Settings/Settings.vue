dc9884fa6ee08f42ca4ab76a2d8eafacd3d8c382
wildone <max.barrass@gmail.com>
2022-05-18 00:14:01 +1000

    use updated services component.

--- src/components/Settings/Settings.vue
+++ src/components/Settings/Settings.vue
@@ -32,18 +32,29 @@
             @click="changeTab(setting.id)"
           >
             <i :class="setting.icon"></i>
-            {{ $t(`components.setting.${setting.id}`) }}
+            {{ $t(`components.setting.section.${setting.id}`) }}
           </li>
         </ul>
       </div>
       <div class="menu-content">
+        <!-- section content -->
+        <!-- general -->
         <h4 v-if="selected == 'general'">
           {{ $t("components.setting.general-settings") }}
         </h4>
-        <profile-setting v-if="selected == 'profile'" :field="path.tabField" />
+        <!-- profile -->
+        <profile-info v-if="selected == 'profile'" :field="path.tabField" />
+
+        <!-- privacy -->
         <h4 v-if="selected == 'privacy'">
           {{ $t("components.setting.privacy-settings") }}
         </h4>
+        <!-- services -->
+        <services-list
+          v-if="selected == 'services'"
+          :variant="'table'"
+          :field="path.tabField"
+        />
       </div>
     </div>
   </Dialog>
@@ -52,18 +63,21 @@
 <script>
   import Dialog from "primevue/dialog"
   import { getModule } from "vuex-module-decorators"
-  import ProfileSetting from "./ProfileSetting.vue"
+  import ProfileInfo from "./Profile.vue"
+  import ServicesList from "../Services"
   import AppSettings from "@/store/Modules/AppSettings"
   const appSettings = getModule(AppSettings)
 
   export default {
     name: "Settings",
-    components: { Dialog, ProfileSetting },
+    components: { Dialog, ProfileInfo, ServicesList },
     emits: ["hide"],
+    data() {
+      return {
+        selected: appSettings.settingPath?.split("/")[0],
+      }
+    },
     computed: {
-      selected() {
-        return appSettings.settingPath?.split("/")[0]
-      },
       settings() {
         return appSettings.settings
       },
