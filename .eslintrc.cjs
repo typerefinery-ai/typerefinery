b6835cc82f4cd2a33ddfabc98998a63e91c05847
wildone <max.barrass@gmail.com>
2022-05-20 01:50:14 +1000

    remove not required exclude rule.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,7 +20,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": 0,
-    "prettier/prettier": ["error", { endOfLine: "off" }],
   },
   overrides: [
     {
