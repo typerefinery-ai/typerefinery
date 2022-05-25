42dd286b881839a9217dc92a069133bb53c73589
Rohit gupta <rogupta@aapnainfotech.com>
2022-05-25 19:12:50 +0530

    Added #85 add login and register page

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,22 @@ import { createRouter, createWebHashHistory } from "vue-router"
 import Home from "../views/Home.vue"
 import Chat from "../components/Chat/index.vue"
 import Maps from "../components/Maps/index.vue"
+import Login from "../components/Pages/Login.vue"
+import SignUp from "../components/Pages/SignUp.vue"
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/sign-up",
+      name: "SignUp",
+      component: SignUp,
+    },
+    {
+      path: "/",
+      name: "Login",
+      component: Login,
+    },
     {
       path: "/home/:id",
       name: "home",
@@ -21,10 +33,10 @@ const router = createRouter({
       name: "chat",
       component: Chat,
     },
-    {
-      path: "/",
-      redirect: "/home/project",
-    },
+    // {
+    //   path: "/",
+    //   redirect: "/home/project",
+    // },
   ],
 })
 
