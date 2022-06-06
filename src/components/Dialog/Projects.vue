1f35f3b1e39d555a2b79d65fbfc131b4fa915fb5
Rohit gupta <rogupta@aapnainfotech.com>
2022-06-06 10:48:16 +0530

    added #89 populate project tree from project store

--- src/components/Dialog/Projects.vue
+++ src/components/Dialog/Projects.vue
@@ -21,25 +21,25 @@
         </button>
       </div>
     </template>
-    <Panel :header="$t(`components.dialog.projects.panelheader`)" >
-    <div class="field">
-      <label for="name">
+    <Panel :header="$t(`components.dialog.projects.panelheader`)">
+      <div class="field">
+        <label for="name">
           {{ $t("components.dialog.projects.info.name") }}</label
-      >
-      <InputText id="name" v-model="name" class="p-invalid"/>
-    </div>
-    <div class="field">
-      <label for="des">
-         {{ $t("components.dialog.projects.info.description") }}</label
-      >
-      <InputText id="des" v-model="des" class="p-invalid"/>
-    </div>
-    <div class="field">
-      <label for="icon">
-        {{ $t("components.dialog.projects.info.icon") }}</label
-      >
-      <InputText id="icon" v-model="icon" class="p-invalid"/>
-    </div>
+        >
+        <InputText id="name" v-model="name" class="p-invalid" />
+      </div>
+      <div class="field">
+        <label for="des">
+          {{ $t("components.dialog.projects.info.description") }}</label
+        >
+        <InputText id="des" v-model="des" class="p-invalid" />
+      </div>
+      <div class="field">
+        <label for="icon">
+          {{ $t("components.dialog.projects.info.icon") }}</label
+        >
+        <InputText id="icon" v-model="icon" class="p-invalid" />
+      </div>
     </Panel>
     <template #footer>
       <Button
@@ -59,83 +59,123 @@
 </template>
 
 <script>
-import Dialog from "primevue/dialog"
-import Dropdown from "primevue/dropdown"
-import Panel from "primevue/panel"
-import InputText from "primevue/inputtext"
-import Button from "primevue/button"
-import Projects from "@/store/Modules/Projects"
-import { getModule } from "vuex-module-decorators"
-const appProjects = getModule(Projects)
+  import Dialog from "primevue/dialog"
+  import Dropdown from "primevue/dropdown"
+  import Panel from "primevue/panel"
+  import InputText from "primevue/inputtext"
+  import Button from "primevue/button"
+  import Projects from "@/store/Modules/Projects"
+  import { getModule } from "vuex-module-decorators"
+  const appProjects = getModule(Projects)
 
-export default {
-  name: "NewProject",
-  components: {
-    Dialog,
-    Panel,
-    InputText,
-    Button,
-    Dropdown,
-  },
-  props: {
-    projectdialog: { type: Boolean, default: false },
-  },
-  data() {
-    return {
-      type: "",
-      name: "",
-      expanded: "",
-      description: "",
-      icon: "",
-      display: true,
-    }
-  },
-  emits: ["close"],
-  methods: {
-    closeDialog() {
-      this.$emit("close")
+  export default {
+    name: "NewProject",
+    components: {
+      Dialog,
+      Panel,
+      InputText,
+      Button,
+      Dropdown,
     },
-    handleProjectstore() {
-      const data = {
-        type: "project",
-        name: this.name,
-        description: this.des,
-        icon: this.icon,
+    props: {
+      projectdialog: { type: Boolean, default: false },
+    },
+    data() {
+      return {
+        type: "",
+        name: "",
+        expanded: "",
+        description: "",
+        icon: "",
+        display: true,
       }
-      appProjects.addToList(data)
-      this.$emit("close")
     },
-  },
-}
+    emits: ["close"],
+    methods: {
+      closeDialog() {
+        this.$emit("close")
+      },
+      handleProjectstore() {
+        const data = {
+          type: "project",
+          name: this.name,
+          description: this.des,
+          icon: this.icon,
+          queries: {
+            type: "queries",
+            expanded: true,
+            list: [
+              {
+                name: "query ",
+                description: "",
+                type: "query",
+                connection: "connection 2",
+                icon: "connection",
+                query: "",
+                transformer: "transformer 1",
+              },
+            ],
+          },
+          connections: {
+            type: "connections",
+            expanded: true,
+            icon: "connection",
+            list: [
+              {
+                name: "connection ",
+                title: "connection 1",
+                icon: "connection",
+                description: "",
+                type: "connection",
+              },
+            ],
+          },
+
+          transformers: {
+            type: "transformers",
+            list: [
+              {
+                name: "Transformer ",
+                title: "Transformer 1",
+                icon: "connection",
+                description: "",
+                type: "transformer",
+              },
+            ],
+          },
+        }
+        appProjects.addToList(data)
+        this.$emit("close")
+      },
+    },
+  }
 </script>
-<style  lang="scss">
-.dropdown1{
-  width: 100% !important;;
-}
-input {
-  width: 80%;
-}
-.field {
-  display: grid;
-}
-.projects-dialog {
-  height: 100vh;
-  width: 40vw;
-  .p-dropdown{
+<style lang="scss">
+  .dropdown1 {
+    width: 100% !important;
+  }
+  input {
     width: 80%;
   }
-  .p-dialog-content {
-    height: 100%;
+  .field {
+    display: grid;
   }
+  .projects-dialog {
+    height: 100vh;
+    width: 40vw;
+    .p-dropdown {
+      width: 80%;
+    }
+    .p-dialog-content {
+      height: 100%;
+    }
 
-  .p-dialog-header {
-    padding: 1.25rem 1.8rem;
+    .p-dialog-header {
+      padding: 1.25rem 1.8rem;
 
-    .p-dialog-header-icons:last-of-type {
-      display: none;
+      .p-dialog-header-icons:last-of-type {
+        display: none;
+      }
     }
   }
-
- 
-}
 </style>
--- src/components/Project/Sidebar/index.vue
+++ src/components/Project/Sidebar/index.vue
@@ -11,24 +11,21 @@
         <tune-icon v-tooltip="$t(`tooltips.settings`)" :size="25" />
       </div>
       <div class="sidebar-fixed-items hover:text-primary">
-        <logout-icon v-on:click="logout" v-tooltip="$t(`tooltips.logout`)" :size="25" />
+        <logout-icon
+          v-on:click="logout"
+          v-tooltip="$t(`tooltips.logout`)"
+          :size="25"
+        />
       </div>
     </div>
     <div id="sidebar-draggable" class="sidebar-draggable">
       <Tree
         class="h-full tree-wrapper"
-        :value="nodes"
+        :value="nodesData"
         :filter="true"
         filter-mode="lenient"
         :expanded-keys="expandedKeys"
       >
-        <template #default="slotProps">
-          {{
-            slotProps.node.id
-              ? $t(`components.project.${slotProps.node.id}`)
-              : slotProps.node.label
-          }}
-        </template>
       </Tree>
     </div>
   </div>
@@ -36,42 +33,95 @@
 
 <script>
   import Tree from "primevue/tree"
-  import NodeService from "@/components/Tree/NodeService"
   import FileIcon from "vue-material-design-icons/FileMultipleOutline.vue"
   import MagnifyIcon from "vue-material-design-icons/Magnify.vue"
   import LogoutIcon from "vue-material-design-icons/Logout.vue"
   import TuneIcon from "vue-material-design-icons/Tune.vue"
   import { getModule } from "vuex-module-decorators"
   import AppSettings from "@/store/Modules/AppSettings"
+  import Projects from "@/store/Modules/Projects"
   const appSettings = getModule(AppSettings)
+  const appProjects = getModule(Projects)
 
   export default {
     name: "Sidebar",
     components: { LogoutIcon, MagnifyIcon, FileIcon, Tree, TuneIcon },
+
     data() {
       return {
-        nodes: null,
         expandedKeys: {},
       }
     },
-    nodeService: null,
-    created() {
-      this.nodeService = new NodeService()
+    computed: {
+      storeList() {
+        return appProjects.storedata
+      },
+      nodesData() {
+        const index = 0
+        return this.storeList.map((el, i) => {
+          console.log(el)
+          return {
+            key: i,
+            id: el.name.toLowerCase(),
+            label: el.name,
+            data: "Project Folder",
+            icon: "pi pi-fw pi-briefcase",
+            children: [
+              {
+                key: `${index}-${i}`,
+                label: "Query",
+                data: "Query Folder",
+                icon: "pi pi-fw pi-database",
+                children: el.queries.list.map((el) => {
+                  return {
+                    key: "0-0-0",
+                    label: el.name,
+                    data: "Query Folder",
+                    icon: "pi pi-fw pi-file",
+                  }
+                }),
+              },
+              {
+                key: `${index + 1}-${i}`,
+                label: "Connection",
+                data: "Connection Folder",
+                icon: "pi pi-fw pi-server",
+                children: el.connections.list.map((el) => {
+                  return {
+                    label: el.name,
+                    data: "Connection Folder",
+                    icon: "pi pi-fw pi-file",
+                  }
+                }),
+              },
+              {
+                key: `${index + 2}-${i}`,
+                label: "Transformer",
+                data: "Transformer Folder",
+                icon: "pi pi-fw pi-chart-bar",
+                children: el.transformers.list.map((el) => {
+                  return {
+                    key: "0-0-i",
+                    label: el.name,
+                    data: "Transformer Folder",
+                    icon: "pi pi-fw pi-file",
+                  }
+                }),
+              },
+            ],
+          }
+        })
+      },
     },
-    mounted() {
-      this.nodes = this.nodeService.getTreeNodes().root
-      this.expandedKeys[this.nodes[0].key] = true
-      this.expandedKeys[this.nodes[1].key] = true
-    },
+
     methods: {
       openSettings() {
         appSettings.openSettingsDialog("general")
       },
-      logout()
-        {
-            localStorage.clear();
-            this.$router.push({name: 'Login'})
-        }
+      logout() {
+        localStorage.clear()
+        this.$router.push({ name: "Login" })
+      },
     },
   }
 </script>
