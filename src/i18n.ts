01c17d12117ffc96b2e65d28b5464421377fddf7
Ankit Vats <avats@aapnainfotech.com>
2022-06-15 15:25:37 +0530

    Add #154 - Override context menu

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,4 @@
-import { createI18n } from "vue-i18n"
+import { createI18n } from "vue-i18n/index"
 import en from "./locales/en.json"
 import hi from "./locales/hi.json"
 import AppSettings from "@/store/Modules/AppSettings"
