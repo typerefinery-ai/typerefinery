2ec900f32b4d01d1ac0516109c3ef1ffe30dc441
wildone <max.barrass@gmail.com>
2022-06-21 01:37:24 +1000

    update utils.

--- electron/app/main/Utils.ts
+++ electron/app/main/Utils.ts
@@ -1,4 +1,5 @@
 import * as config from "../../../package.json"
+import portfinder from "portfinder"
 
 export function tryParseInt(text: string, defaultValue: number): number {
   try {
@@ -17,4 +18,24 @@ export function getEnvConfigWithDefault(
   defaultValue: any = undefined
 ): any {
   return process.env[key] || config.env[key] || defaultValue
+}
+
+export async function getPortFree(port: number, host: string) {
+  // return new Promise((res) => {
+  //   const srv = net.createServer()
+  //   srv.listen(0, () => {
+  //     const port: AddressInfo = srv.address() as AddressInfo
+  //     srv.close((err) => res(port.port))
+  //   })
+  // })
+  return portfinder
+    .getPortPromise({ host: host, port: port })
+    .then((port) => {
+      return port
+    })
+    .catch((err) => {
+      //
+      // Could not get a free port, `err` contains the reason.
+      //
+    })
 }
