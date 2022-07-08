4b460083c3367ec117b51306215a88703ba13b02
Shivam-apna <sksingh@aapnainfotech.com>
2022-07-08 19:09:09 +0530

    Updated Function test

--- cypress/integration/projects/TabSubMenu.spec.ts
+++ cypress/integration/projects/TabSubMenu.spec.ts
@@ -27,28 +27,4 @@ describe("Projecty Tab", () => {
     cy.get(`[data-v-6ac6996e]`).contains("query 1").dblclick()
     cy.get(".p-tree-toggler-icon").eq(2).click()
   })
-
-  it("Has LocalConnection Button", () => {
-    cy.get(".p-tree-toggler-icon").eq(3).click()
-    cy.get(`[data-v-6ac6996e]`).contains("connection 1").dblclick()
-    cy.get(".p-dialog-title").contains("Update Connection")
-    cy.get(".p-button-label").contains("Cancel").click()
-    cy.get(".p-tree-toggler-icon").eq(3).click()
-  })
-  it("Has LocalTransformer Button", () => {
-    cy.get(".p-tree-toggler-icon").eq(4).click()
-    cy.get(`[data-v-6ac6996e]`).contains("transformer 1").dblclick()
-    cy.get(".p-dialog-title").contains("Update Transformer")
-    cy.get(".p-button-label").contains("Cancel").click()
-    cy.get(".p-tree-toggler-icon").eq(4).click()
-  })
-  it("Has LocalAlgorithm Button", () => {
-    cy.get(".p-tree-toggler-icon").eq(5).click()
-    cy.get(`[data-v-6ac6996e]`).contains("algorithm 1").dblclick()
-    cy.get(".p-dialog-title").contains("Update Algorithm")
-    cy.get(".p-button-label").contains("Cancel").click()
-    cy.get(".p-tree-toggler-icon").eq(5).click()
-    cy.get(".p-tree-toggler-icon").eq(1).click()
-    cy.get(".p-tree-toggler-icon").eq(0).click()
-  })
 })
