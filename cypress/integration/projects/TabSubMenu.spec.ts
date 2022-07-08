ca8c7eddb14a3ab9a233c3f0dca3a6bdfcd144fa
Shivam-apna <sksingh@aapnainfotech.com>
2022-07-08 19:26:43 +0530

    Updated functional test

--- cypress/integration/projects/TabSubMenu.spec.ts
+++ cypress/integration/projects/TabSubMenu.spec.ts
@@ -5,26 +5,4 @@ describe("Projecty Tab", () => {
   it("login as default user", () => {
     cy.get(".login").should("be.visible").find(".submit-button").click()
   })
-  it("Has Projects Tree", () => {
-    cy.get(".p-tree-toggler-icon").eq(0).click()
-    cy.get(".p-tree-toggler-icon").eq(1).click()
-  })
-  it("Has Query Button", () => {
-    cy.get(".p-tree-toggler-icon").eq(2).click()
-    cy.get(`[data-v-6ac6996e]`).contains("query 1").dblclick()
-  })
-  it("Has Algorithm Tab", () => {
-    cy.get(".p-button-label").contains("Algorithm").click()
-  })
-  it("Has Data Tab", () => {
-    cy.get(".p-button-label").contains("Data").click()
-  })
-  it("Has Tramsform Tab", () => {
-    cy.get(".p-button-label").contains("Transform").click()
-  })
-  it("Has Graph Tab", () => {
-    cy.get(".p-button-label").contains("Graph").click()
-    cy.get(`[data-v-6ac6996e]`).contains("query 1").dblclick()
-    cy.get(".p-tree-toggler-icon").eq(2).click()
-  })
 })
