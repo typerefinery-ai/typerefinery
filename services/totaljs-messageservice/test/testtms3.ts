b34883f79772cb05e3b19df15fa094dde3da04e6
wildone <max.barrass@gmail.com>
2022-08-29 22:07:20 +1000

    add sample tms ws.

---
+++ services/totaljs-messageservice/test/testtms3.ts
@@ -0,0 +1,30 @@
+// import websocket = require('websocket');
+import * as websocket from 'websocket';
+import * as http from 'http';
+
+//connect to the server ws://localhost:8112/$tms/
+var client = new websocket.client();
+client.connect('ws://localhost:8112/$tms/');
+client.on('connect', function (connection) {
+    console.log('WebSocket Client Connected');
+    connection.on('error', function (error) {
+        console.log("Connection Error: " + error.toString());
+    });
+    connection.on('close', function () {
+        console.log('echo-protocol Connection Closed');
+    });
+    connection.on('message', function (message) {
+        console.log("Received: '" + message + "'");
+        // if (message.type === 'utf8') {
+        //     console.log("Received: '" + message.utf8Data + "'");
+        // }
+    });
+    function sendNumber() {
+        if (connection.connected) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            connection.sendUTF(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+});
