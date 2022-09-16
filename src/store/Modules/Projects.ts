b181a0cacb78db32af5dcf7de602a98b3997bca2
Ankit Vats <avats@aapnainfotech.com>
2022-09-16 13:10:09 +0530

    Minor fixes

--- src/store/Modules/Projects.ts
+++ src/store/Modules/Projects.ts
@@ -36,54 +36,54 @@ export default class Projects extends VuexModule {
     }
   }
 
-  get getLocalTransformers() {
-    return (projectIdx) => {
-      return this.data.list[projectIdx].transformers.list
-    }
-  }
+  // get getLocalTransformers() {
+  //   return (projectIdx) => {
+  //     return this.data.list[projectIdx].transformers.list
+  //   }
+  // }
 
-  get getLocalAlgorithms() {
-    return (projectIdx) => {
-      return this.data.list[projectIdx].algorithms.list
-    }
-  }
+  // get getLocalAlgorithms() {
+  //   return (projectIdx) => {
+  //     return this.data.list[projectIdx].algorithms.list
+  //   }
+  // }
 
   // Query Transformer
-  get getTransformerCode() {
-    return (projectIdx: number, queryIdx: number) => {
-      return this.data.list[projectIdx].queries.list[queryIdx].transformer.code
-    }
-  }
+  // get getTransformerCode() {
+  //   return (projectIdx: number, queryIdx: number) => {
+  //     return this.data.list[projectIdx].queries.list[queryIdx].transformer.code
+  //   }
+  // }
 
-  get getTransformerError() {
-    return (projectIdx: number, queryIdx: number) => {
-      return this.data.list[projectIdx].queries.list[queryIdx].transformer.error
-    }
-  }
+  // get getTransformerError() {
+  //   return (projectIdx: number, queryIdx: number) => {
+  //     return this.data.list[projectIdx].queries.list[queryIdx].transformer.error
+  //   }
+  // }
 
-  get getTransformerConsoleMessage() {
-    return (projectIdx: number, queryIdx: number) => {
-      let myString = ""
-      this.data.list[projectIdx].queries.list[
-        queryIdx
-      ].transformer.logs.forEach((el, i) => {
-        if (i === 0) myString = JSON.stringify(el)
-        else myString = myString + "\n" + JSON.stringify(el)
-      })
-      return (
-        myString +
-        "\n" +
-        this.data.list[projectIdx].queries.list[queryIdx].transformer.error
-      )
-    }
-  }
+  // get getTransformerConsoleMessage() {
+  //   return (projectIdx: number, queryIdx: number) => {
+  //     let myString = ""
+  //     this.data.list[projectIdx].queries.list[
+  //       queryIdx
+  //     ].transformer.logs.forEach((el, i) => {
+  //       if (i === 0) myString = JSON.stringify(el)
+  //       else myString = myString + "\n" + JSON.stringify(el)
+  //     })
+  //     return (
+  //       myString +
+  //       "\n" +
+  //       this.data.list[projectIdx].queries.list[queryIdx].transformer.error
+  //     )
+  //   }
+  // }
 
-  // Query Algorithm
-  get getAlgorithmCode() {
-    return (projectIdx: number, queryIdx: number) => {
-      return this.data.list[projectIdx].queries.list[queryIdx].algorithm.code
-    }
-  }
+  // // Query Algorithm
+  // get getAlgorithmCode() {
+  //   return (projectIdx: number, queryIdx: number) => {
+  //     return this.data.list[projectIdx].queries.list[queryIdx].algorithm.code
+  //   }
+  // }
 
   /**** Mutations ****/
   @Mutation
@@ -149,33 +149,33 @@ export default class Projects extends VuexModule {
     this.data.list = projects
   }
 
-  @Mutation
-  addLocalTransformer(transformerData) {
-    const { projectIdx, data } = transformerData
-    this.data.list[projectIdx].transformers.list.push(data)
-  }
+  // @Mutation
+  // addLocalTransformer(transformerData) {
+  //   const { projectIdx, data } = transformerData
+  //   this.data.list[projectIdx].transformers.list.push(data)
+  // }
 
-  @Mutation
-  editLocalTransformer(transformerData) {
-    const { projectIdx, transformerIdx, data } = transformerData
-    const projects = JSON.parse(JSON.stringify(this.data.list))
-    projects[projectIdx].transformers.list[transformerIdx] = data
-    this.data.list = projects
-  }
+  // @Mutation
+  // editLocalTransformer(transformerData) {
+  //   const { projectIdx, transformerIdx, data } = transformerData
+  //   const projects = JSON.parse(JSON.stringify(this.data.list))
+  //   projects[projectIdx].transformers.list[transformerIdx] = data
+  //   this.data.list = projects
+  // }
 
-  @Mutation
-  addLocalAlgorithm(algorithmData) {
-    const { projectIdx, data } = algorithmData
-    this.data.list[projectIdx].algorithms.list.push(data)
-  }
+  // @Mutation
+  // addLocalAlgorithm(algorithmData) {
+  //   const { projectIdx, data } = algorithmData
+  //   this.data.list[projectIdx].algorithms.list.push(data)
+  // }
 
-  @Mutation
-  editLocalAlgorithm(algorithmData) {
-    const { projectIdx, algorithmIdx, data } = algorithmData
-    const projects = JSON.parse(JSON.stringify(this.data.list))
-    projects[projectIdx].algorithms.list[algorithmIdx] = data
-    this.data.list = projects
-  }
+  // @Mutation
+  // editLocalAlgorithm(algorithmData) {
+  //   const { projectIdx, algorithmIdx, data } = algorithmData
+  //   const projects = JSON.parse(JSON.stringify(this.data.list))
+  //   projects[projectIdx].algorithms.list[algorithmIdx] = data
+  //   this.data.list = projects
+  // }
 
   @Mutation
   setQueryDataPath({ path, projectIdx, queryIdx }) {
@@ -185,73 +185,73 @@ export default class Projects extends VuexModule {
   }
 
   // Query Transformer
-  @Mutation
-  setTransformerCode({ code, projectIdx, queryIdx }) {
-    const projects = JSON.parse(JSON.stringify(this.data.list))
-    projects[projectIdx].queries.list[queryIdx].transformer.code = code
-    this.data.list = projects
-  }
+  // @Mutation
+  // setTransformerCode({ code, projectIdx, queryIdx }) {
+  //   const projects = JSON.parse(JSON.stringify(this.data.list))
+  //   projects[projectIdx].queries.list[queryIdx].transformer.code = code
+  //   this.data.list = projects
+  // }
 
-  @Mutation
-  setTransformerError({ error, projectIdx, queryIdx }) {
-    const projects = JSON.parse(JSON.stringify(this.data.list))
-    projects[projectIdx].queries.list[queryIdx].transformer.error = error
-    this.data.list = projects
-  }
+  // @Mutation
+  // setTransformerError({ error, projectIdx, queryIdx }) {
+  //   const projects = JSON.parse(JSON.stringify(this.data.list))
+  //   projects[projectIdx].queries.list[queryIdx].transformer.error = error
+  //   this.data.list = projects
+  // }
 
-  @Mutation
-  setTransformerLogs({ log, projectIdx, queryIdx }) {
-    let logs: string
-    if (typeof log == "object") {
-      logs = JSON.stringify(log)
-    } else if (typeof log != "string") return
-    logs = log
-    const projects = JSON.parse(JSON.stringify(this.data.list))
-    projects[projectIdx].queries.list[queryIdx].transformer.logs.push(logs)
-    this.data.list = projects
-  }
+  // @Mutation
+  // setTransformerLogs({ log, projectIdx, queryIdx }) {
+  //   let logs: string
+  //   if (typeof log == "object") {
+  //     logs = JSON.stringify(log)
+  //   } else if (typeof log != "string") return
+  //   logs = log
+  //   const projects = JSON.parse(JSON.stringify(this.data.list))
+  //   projects[projectIdx].queries.list[queryIdx].transformer.logs.push(logs)
+  //   this.data.list = projects
+  // }
 
-  @Mutation
-  clearTransformerLogs({ projectIdx, queryIdx }) {
-    const projects = JSON.parse(JSON.stringify(this.data.list))
-    projects[projectIdx].queries.list[queryIdx].transformer.logs = []
-    this.data.list = projects
-  }
+  // @Mutation
+  // clearTransformerLogs({ projectIdx, queryIdx }) {
+  //   const projects = JSON.parse(JSON.stringify(this.data.list))
+  //   projects[projectIdx].queries.list[queryIdx].transformer.logs = []
+  //   this.data.list = projects
+  // }
 
-  // Query Algorithm
-  @Mutation
-  setAlgoCode({ code, projectIdx, queryIdx }) {
-    const projects = JSON.parse(JSON.stringify(this.data.list))
-    projects[projectIdx].queries.list[queryIdx].algorithm.code = code
-    this.data.list = projects
-  }
+  // // Query Algorithm
+  // @Mutation
+  // setAlgoCode({ code, projectIdx, queryIdx }) {
+  //   const projects = JSON.parse(JSON.stringify(this.data.list))
+  //   projects[projectIdx].queries.list[queryIdx].algorithm.code = code
+  //   this.data.list = projects
+  // }
 
   /**** Actions ****/
-  @Action
-  async runAlgorithm(indexes) {
-    try {
-      const { projectIdx, queryIdx } = indexes
-      const query = this.getQuery(projectIdx, queryIdx)
-      const payload = {
-        dbhost: query.connection.host,
-        dbport: query.connection.port,
-        dbdatabase: query.database,
-        dbquery: query.query,
-        algorithm: query.algorithm.code,
-        algorithmrequirements: "argparse\nloguru",
-        returnoutput: "log",
-      }
-      const response = await axios.post(query.endpoint, payload)
-      const outputExists =
-        response.headers["output.exists"].toLowerCase() == "true"
-      let path = ""
-      if (outputExists) path = response.headers["output.url"]
-      const data = { path, projectIdx, queryIdx }
-      this.setQueryDataPath(data)
-      return { data: response.data, type: "data" }
-    } catch (error) {
-      console.log(error)
-      return { data: error, type: "error" }
-    }
-  }
+  // @Action
+  // async runAlgorithm(indexes) {
+  //   try {
+  //     const { projectIdx, queryIdx } = indexes
+  //     const query = this.getQuery(projectIdx, queryIdx)
+  //     const payload = {
+  //       dbhost: query.connection.host,
+  //       dbport: query.connection.port,
+  //       dbdatabase: query.database,
+  //       dbquery: query.query,
+  //       algorithm: query.algorithm.code,
+  //       algorithmrequirements: "argparse\nloguru",
+  //       returnoutput: "log",
+  //     }
+  //     const response = await axios.post(query.endpoint, payload)
+  //     const outputExists =
+  //       response.headers["output.exists"].toLowerCase() == "true"
+  //     let path = ""
+  //     if (outputExists) path = response.headers["output.url"]
+  //     const data = { path, projectIdx, queryIdx }
+  //     this.setQueryDataPath(data)
+  //     return { data: response.data, type: "data" }
+  //   } catch (error) {
+  //     console.log(error)
+  //     return { data: error, type: "error" }
+  //   }
+  // }
 }
