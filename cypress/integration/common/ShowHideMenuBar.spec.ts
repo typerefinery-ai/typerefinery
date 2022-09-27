a385b8ca9adb161fba2d1bd95d0a1a906ff8d262
Shivam kumar Singh <sksingh@aapnainfotech.com>
2022-09-27 19:35:36 +0530

    fixed functional test

--- cypress/integration/common/ShowHideMenuBar.spec.ts
+++ cypress/integration/common/ShowHideMenuBar.spec.ts
@@ -2,6 +2,9 @@ describe("Projecty Tab", () => {
   it("visits the app root url", () => {
     cy.visit("/")
   })
+  it("Has popUp", () => {
+    cy.get(".p-button-text").click()
+  })
   it("Has hide menu button", () => {
     cy.get(".main-submenu > .icon-wrapper > .pi").click()
   })
--- cypress/integration/projects/TabSubMenu.spec.ts
+++ cypress/integration/projects/TabSubMenu.spec.ts
@@ -2,4 +2,7 @@ describe("Projecty Tab", () => {
   it("visits the app root url", () => {
     cy.visit("/")
   })
+  it("Has popUp", () => {
+    cy.get(".p-button-text").click()
+  })
 })
