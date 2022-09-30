35b2421ffe9739f03b9643214ca168a746fcdd62
Rohit gupta <rogupta@aapnainfotech.com>
2022-09-30 15:39:36 +0530

    remove sample data popup on refresh and add alert for delete project

---
+++ src/components/Dialog/DeleteProjectPopup.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <Dialog
+      v-model:visible="displayDeletePopup"
+      :header="$t(`components.dialog.projects.info.delete-project`)"
+      :modal="true"
+      :closable="true"
+      :style="{ width: '350px' }"
+    >
+      <div class="confirmation-content">
+        <i class="pi pi-info-circle mr-3" style="font-size: 2rem" />
+        <span>{{
+          $t(`components.dialog.projects.info.delete-project-popup`)
+        }}</span>
+      </div>
+
+      <template #footer>
+        <Button
+          :label="$t(`components.dialog.projects.info.cancel`)"
+          icon="pi pi-times"
+          class="p-button-text"
+          @click="closeDeleteDialog"
+        />
+        <Button
+          id="deleteTreeNode"
+          :label="$t(`components.dialog.projects.info.delete`)"
+          :icon="`pi ${loading ? 'pi-spin pi-spinner' : 'pi-check'}`"
+          :style="{ 'pointer-events': loading ? 'none' : 'auto' }"
+          class="p-button-text"
+          @click="deleteProjectTreenode"
+        />
+      </template>
+    </Dialog>
+  </div>
+</template>
+
+<script>
+  import { getModule } from "vuex-module-decorators"
+  import Dialog from "primevue/dialog"
+  import Button from "primevue/button"
+  import Projects from "@/store/Modules/Projects"
+  import AppData from "@/store/Modules/AppData"
+  const projectsModule = getModule(Projects)
+  const appDataModule = getModule(AppData)
+
+  export default {
+    name: "DeleteTreeNodePopup",
+    components: {
+      Dialog,
+      Button,
+    },
+    props: {
+      node: { type: Object, required: true },
+      deletedialog: { type: Boolean, default: false },
+    },
+    data() {
+      return {
+        displayDeletePopup: true,
+        loading: false,
+      }
+    },
+    methods: {
+      closeDeleteDialog() {
+        this.displayDeletePopup = !this.displayDeletePopup
+      },
+      async deleteProjectTreenode() {
+        const nodeData = this.node
+        if (nodeData.type == "project") {
+          const payload = {
+            key: nodeData.key,
+            id: nodeData.id,
+            ...this.tab,
+          }
+          const eKeys = [nodeData.key]
+          const sKeys = [nodeData.key]
+          const ids = [nodeData.id]
+          nodeData.children.forEach((el) => {
+            eKeys.push(el.key)
+            el.children.forEach((el) => {
+              sKeys.push(el.key)
+              ids.push(el.id)
+            })
+          })
+
+          this.loading = true
+          appDataModule.removeSelectedTreeNodes(ids)
+          appDataModule.toggleTreeNode()
+          projectsModule.removeExpandedNodesByKeys(eKeys)
+          projectsModule.removeSelectedNodesByKeys(sKeys)
+          projectsModule.deleteProjectData(payload)
+          this.displayDeletePopup = !this.displayDeletePopup
+        }
+      },
+    },
+  }
+</script>
+<style scoped lang="scss">
+  p {
+    margin: 0;
+  }
+
+  .confirmation-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .p-dialog .p-button {
+    min-width: 6rem;
+  }
+
+  #deleteTreeNode {
+    color: white;
+    background-color: #ef4444;
+  }
+</style>
