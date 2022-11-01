c294f69c99f26f184f717e0824f0351cd4c2470c
Ankit Vats <avats@aapnainfotech.com>
2022-11-01 11:25:56 +0530

    Add toast notification

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import store from "./store"
 import { loadFonts } from "./plugins/webfontloader"
 import Vuelidate from "vuelidate"
 import ConfirmationService from "primevue/confirmationservice"
+import ToastService from "primevue/toastservice"
 
 loadFonts()
 
@@ -20,5 +21,6 @@ app.use(store)
 app.use(PrimeVue)
 app.directive("tooltip", Tooltip)
 app.use(ConfirmationService)
+app.use(ToastService)
 app.mount("#app")
 app.component("InputText", InputText)
