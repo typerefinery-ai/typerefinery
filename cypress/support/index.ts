78984450bdde9aad937c3d984a847e685b54abbf
Shivam-apna <sksingh@aapnainfotech.com>
2022-11-07 17:37:56 +0530

    Added check for uncaugh:exception.

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -19,3 +19,7 @@ import "./report"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from failing the test
+  return false
+})
