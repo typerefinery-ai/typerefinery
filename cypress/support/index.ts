8be699099ff00d17ffbb809702514399a23cf06d
Shivam-apna <sksingh@aapnainfotech.com>
2022-11-07 18:12:07 +0530

    updated cypress/support/index.ts.

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -19,7 +19,3 @@ import "./report"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-Cypress.on("uncaught:exception", (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
