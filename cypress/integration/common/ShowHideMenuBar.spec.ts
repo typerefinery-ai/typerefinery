5b4ec5db510621cf854960775978fe917183a67f
Shivam-apna <sksingh@aapnainfotech.com>
2022-11-09 17:08:53 +0530

    updated delayed command .

--- cypress/integration/common/ShowHideMenuBar.spec.ts
+++ cypress/integration/common/ShowHideMenuBar.spec.ts
@@ -1,16 +1,16 @@
 describe("Project Tab", () => {
   it("visits the app root url", () => {
-    cy.visit("/");
+    cy.visit("/")
   })
   // it("Has popUp", () => {
   //   cy.get(".p-button-text").click()
   // })
-  
+
   // Hide the bar
   it("Has hide menu button", () => {
     cy.get(".main-submenu > .icon-wrapper > .pi").click()
   })
-  it("Has Show Menubar button", () => {
-    cy.get("#show-menu-bar-button").click()
-  })
+  // it("Has Show Menubar button", () => {
+  //   cy.get("#show-menu-bar-button").click()
+  // })
 })
