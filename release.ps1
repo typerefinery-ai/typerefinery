917dd9dcd996a97c2774b69c313dfee0d6c598d7
wildone <max.barrass@gmail.com>
2023-04-01 21:08:51 +1100

    chore: release v2023.3.31
    Signed-off-by: wildone <max.barrass@gmail.com>

--- release.ps1
+++ release.ps1
@@ -2,6 +2,11 @@ Param(
   [string]$VERSION = "2023.3.31"
 )
 
+# delete old tag
+git tag -d v${VERSION}
+git push --delete origin v${VERSION}
+
+# create new tag
 npm version ${VERSION}
 git commit -s -a -m "chore: release v${VERSION}"
 git tag -s -a v${VERSION} -m "v${VERSION}4"
