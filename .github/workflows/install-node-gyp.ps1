dfcdc528f89df8c8d3c5a4b270c6594fedbbac17
wildone <max.barrass@gmail.com>
2023-05-15 12:30:04 +1000

    add node-gyp.

---
+++ .github/workflows/install-node-gyp.ps1
@@ -0,0 +1,11 @@
+#   $WhereNode = Get-Command node | Select-Object -ExpandProperty Definition
+#   $NodeDirPath = Split-Path $WhereNode -Parent
+#   $NodeModulesPath = $NodeDirPath + "\node_modules\npm\node_modules\npm-lifecycle"
+#   cd $NodeModulesPath
+#   npm install node-gyp@8.x
+npm install -g node-gyp
+npx node-gyp --verbose list
+npx node-gyp --verbose install $(node -v)
+npx node-gyp --verbose list
+# TODO resolve "node-gyp" cache path or set it for "node-gyp" explicitly rather than hardcoding the value
+ls "C:\Users\runneradmin\AppData\Local\node-gyp\Cache\$($(node -v).TrimStart('v'))\include\node"
--- myBeforePackHook.js
+++
@@ -1,9 +0,0 @@
-exports.default = async function (context) {
-  const fsExtra = require("fs-extra")
-  // NOTE: ONLY FOR WINDOWS
-  const localPath =
-    "C:/Users/" +
-    require("os").userInfo().username +
-    "/AppData/Roaming/TypeRefinery/Local Storage"
-  fsExtra.emptyDirSync(localPath)
-}
