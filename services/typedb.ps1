ffc66f635eb90cdaed13c34def74d95d0a03470b
wildone <max.barrass@gmail.com>
2023-09-18 21:15:37 +1000

    update typedb helper script.

--- services/typedb.ps1
+++ services/typedb.ps1
@@ -3,11 +3,21 @@ Param(
   [string]$SERVICE_NAME = "typedb",
   [string]$CURRENT_PATH = "${PWD}",
   [string]$OS = ( $IsWindows ? "win32" : ( $IsMacOS ? "darwin" : "linux" ) ),
+  [string]$SERVER_JAVAOPTS = "-Xms256m -Xmx1024m",
+  [string]$SERVICE_PORT = "8729",
   [string]$CPU_ARCH = "x64",
-  [string]$JAVA_HOME = ( Join-Path "${PWD}" "_java" "${OS}"  "jre17"),
-  [string]$SERVICE_HOME = ( Join-Path "${PWD}" "${SERVICE_NAME}" "$OS"),
+  [string]$JAVA_HOME = ( Join-Path "${PWD}" "_java" "${OS}" ),
+  [string]$SERVICE_HOME = ( Join-Path "${PWD}" "${SERVICE_NAME}" "$OS" "server"),
   [string]$SERVER_HOME = ( Join-Path "${PWD}" "${SERVICE_NAME}" "$OS"),
-  [string]$JAVA_PROGRAM_PATH = ( Join-Path "${JAVA_HOME}" "bin" "java"),
+  [string]$SERVICE_PLATFORM_HOME = "typedb-all-${NODE_OS}-2.22.0",
+  [string]$SERVICE_PLATFORM_ARCHIVE = ( $IsWindows ? "${SERVICE_PLATFORM_HOME}.7z.001" : "${SERVICE_PLATFORM_HOME}.7z.001" ) ,
+  [string]$SERVICE_DATA_PATH = ( Join-Path "${PWD}" "${SERVICE_NAME}" "server" "data"),
+  [string]$SERVICE_CONFIG_PATH = ( Join-Path "${SERVER_HOME}" "server" "conf" "config.yml" ),
+  [string]$JAVA_PROGRAM_PATH = ( Join-Path "${JAVA_HOME}" "bin" "java.exe"),
+  [string]$ARCHIVE_HOME = ( Join-Path "${PWD}" "_archive"),
+  [string]$ARCHIVE_PROGRAM = ( $IsWindows ? "7za.exe" : "7zz" ),
+  [string]$ARCHIVE_PROGRAM_PATH = ( Join-Path "${PWD}" "_archive" "$OS" "${ARCHIVE_PROGRAM}" ),
+  [switch]$SETUP = $false,
   [switch]$CONSOLE = $false,
   [switch]$PROD = $false,
   [switch]$DEBUG = $false
@@ -27,6 +37,7 @@ Function PrintInfo
   printSectionLine "SERVICE_HOME: ${SERVICE_HOME}"
   printSectionLine "SERVER_HOME: ${SERVER_HOME}"
   printSectionLine "JAVA_PROGRAM_PATH: ${JAVA_PROGRAM_PATH}"
+  printSectionLine "SERVICE_DATA_PATH: ${SERVICE_DATA_PATH}"
   printSectionLine "CONSOLE: ${CONSOLE}"
   printSectionLine "PROD: ${PROD}"
   printSectionLine "DEBUG: ${DEBUG}"
@@ -66,13 +77,18 @@ Function GetAppDataPath {
 
 Function StartServer
 {
+  PrintInfo
 
   Set-Location -Path "${SERVER_HOME}"
   try {
 
-    SetEnvPath "G_CP" ( Join-Path "${SERVER_HOME}" "server" "conf" ) ( Join-Path "${SERVER_HOME}" "server" "lib" "common" "*" ) ( Join-Path "${SERVER_HOME}" "server" "lib" "prod" "*" )
+    SetEnvPath "G_CP" ( Join-Path "${SERVER_HOME}" "server" "lib" "*" )
 
-    Invoke-Expression -Command "${JAVA_PROGRAM_PATH} ${env:SERVER_JAVAOPTS} -cp ""${env:G_CP}"" -D""typedb.dir=${SERVER_HOME}"" ""com.vaticle.typedb.core.server.TypeDBServer"" --storage.data=""${SERVICE_STORAGE_DATA_PATH}"""
+    $COMMAND_START = "${JAVA_PROGRAM_PATH} ${SERVER_JAVAOPTS} -cp ""${env:G_CP}"" -D""typedb.dir=${SERVICE_HOME}"" ""com.vaticle.typedb.core.server.TypeDBServer"" --config=""${SERVICE_CONFIG_PATH}"" --storage.data=""${SERVICE_DATA_PATH}"" --server.address 0.0.0.0:${SERVICE_PORT}"
+    # Invoke-Expression -Command "${JAVA_PROGRAM_PATH} --version"
+    # echo $PWD
+    echo $COMMAND_START
+    Invoke-Expression -Command "${COMMAND_START}"
     # java ${env:SERVER_JAVAOPTS} -cp "${env:G_CP}" -D"typedb.dir=${SERVER_HOME}" "com.vaticle.typedb.core.server.TypeDBServer" --storage.data="${SERVICE_STORAGE_DATA_PATH}"
 
   } finally {
@@ -88,7 +104,7 @@ Function StartCluster
 
     SetEnvPath "G_CP" ( Join-Path "${SERVER_HOME}" "server" "conf" ) ( Join-Path "${SERVER_HOME}" "server" "lib" "common" "*" ) ( Join-Path "${SERVER_HOME}" "server" "lib" "prod" "*" )
 
-    java ${env:SERVER_JAVAOPTS} -cp "${env:G_CP}" -D"typedb.dir=${SERVER_HOME}" "com.vaticle.typedb.core.server.TypeDBClusterServer" --storage.data="${SERVICE_STORAGE_DATA_PATH}"
+    java ${env:SERVER_JAVAOPTS} -cp "${env:G_CP}" -D"typedb.dir=${SERVICE_DATA_PATH}" "com.vaticle.typedb.core.server.TypeDBClusterServer"  --config=""${SERVICE_CONFIG_PATH}"" --storage.data="${SERVICE_STORAGE_DATA_PATH}"
 
   } finally {
     Set-Location -Path "${CURRENT_PATH}"
@@ -104,7 +120,7 @@ Function StartServerProd
 
     SetEnvPath "G_CP" ( Join-Path "${SERVER_HOME}" "server" "conf" ) ( Join-Path "${SERVER_HOME}" "server" "lib" "common" "*" ) ( Join-Path "${SERVER_HOME}" "server" "lib" "prod" "*" )
 
-    java ${env:SERVER_JAVAOPTS} -cp "${env:G_CP}" -D"typedb.dir=${SERVER_HOME}" "com.vaticle.typedb.core.server.TypeDBServer" --storage.data="${SERVICE_STORAGE_DATA_PATH}"
+    java ${env:SERVER_JAVAOPTS} -cp "${env:G_CP}" -D"typedb.dir=${SERVER_HOME}" "com.vaticle.typedb.core.server.TypeDBServer"  --config=""${SERVICE_CONFIG_PATH}"" --storage.data="${SERVICE_STORAGE_DATA_PATH}"
 
   } finally {
     Set-Location -Path "${CURRENT_PATH}"
@@ -117,24 +133,38 @@ Function StartConsole
   Set-Location -Path "${SERVER_HOME}"
   try {
 
-    SetEnvPath "G_CP" ( Join-Path "${SERVER_HOME}" "console" "conf" ) ( Join-Path "${SERVER_HOME}" "console" "lib" "*" )
+    SetEnvPath "G_CP" ( Join-Path "${SERVER_HOME}" "console" "lib" "*" )
 
     printSectionLine "G_CP: ${env:G_CP}"
 
-    java ${env:CONSOLE_JAVAOPTS} -cp "${env:G_CP}" -D"typedb.dir=${SERVER_HOME}" "com.vaticle.typedb.console.TypeDBConsole"
+    java ${env:CONSOLE_JAVAOPTS} -cp "${env:G_CP}" -D"org.jline.terminal.dumb=true"  -D"typedb.dir=${SERVER_HOME}" "com.vaticle.typedb.console.TypeDBConsole" --server 0.0.0.0:${SERVICE_PORT}
 
   } finally {
     Set-Location -Path "${CURRENT_PATH}"
   }
 }
 
+Function StartSetup
+{
+  Set-Location -Path ${SERVER_HOME}
+  if ($IsWindows) {
+    Invoke-Expression -Command "${ARCHIVE_PROGRAM_PATH} -?"
+    Invoke-Expression -Command "${ARCHIVE_PROGRAM_PATH} x -aoa ${SERVICE_PLATFORM_ARCHIVE}"
+  } else {
+    Invoke-Expression -Command "${ARCHIVE_PROGRAM_PATH} -?"
+    Invoke-Expression -Command "${ARCHIVE_PROGRAM_PATH} x -aoa ${SERVICE_PLATFORM_ARCHIVE}"
+    # Invoke-Expression -Command "${ARCHIVE_PROGRAM_PATH} x -y ${SERVICE_PLATFORM_ARCHIVE}"
+    # Invoke-Expression -Command "${ARCHIVE_PROGRAM_PATH} x -aoa ${SERVICE_PLATFORM_HOME}.tar -bb3"
+  }
+  Set-Location -Path "${CURRENT_PATH}"
+}
 
 
 PrintInfo
 
 printSectionBanner "Starting ${SERVICE_NAME} service"
 
-$SERVICE_DATA_PATH = GetAppDataPath
+#$SERVICE_DATA_PATH = GetAppDataPath
 
 if ( $PROD ) {
   $SERVICE_STORAGE_DATA_PATH = ${SERVICE_DATA_PATH}
@@ -153,7 +183,9 @@ printSectionLine "SERVICE_STORAGE_DATA_PATH: ${env:SERVICE_STORAGE_DATA_PATH}"
 
 Invoke-Expression -Command "java --version"
 
-if ( $CONSOLE ) {
+if ( $SETUP ) {
+  StartSetup
+} elseif ( $CONSOLE ) {
   StartConsole
 } elseif ( $PROD ) {
   StartServerProd
