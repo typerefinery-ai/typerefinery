d30b104c9477be6417b110e8a7dd3fb9cf2be3d1
wildone <max.barrass@gmail.com>
2023-10-27 09:24:38 +1100

    update postgre wrapper.

--- services/postgredb-admin/main.py
+++ services/postgredb-admin/main.py
@@ -1,3 +1,12 @@
+# allow importing og service local packages
+import os
+import sys
+
+where_am_i = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, where_am_i+"/__packages__")
+sys.path.append(where_am_i)
+# end of local package imports
+
 import sys
 import os
 import builtins
