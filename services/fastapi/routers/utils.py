880ba7589799abf64139de706124f94779eb8afd
wildone <max.barrass@gmail.com>
2024-01-24 11:11:49 +1100

    add post to string2json.

--- services/fastapi/routers/utils.py
+++ services/fastapi/routers/utils.py
@@ -8,3 +8,8 @@ router = APIRouter()
 @router.get("/string2json")
 async def convert_string2json(source: str = Form(...)):
   return Response(content=json.dumps(source), media_type="text/plain")
+
+@Logger.catch
+@router.post("/string2json")
+async def convert_string2json_post(source: str = Form(...)):
+  return Response(content=json.dumps(source), media_type="text/plain")
