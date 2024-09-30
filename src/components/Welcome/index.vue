7b59ca921c6fe813f594b155be8a6c823d2d022b
wildone <max.barrass@gmail.com>
2024-10-01 01:48:31 +1000

    update welcome to show all services.

--- src/components/Welcome/index.vue
+++ src/components/Welcome/index.vue
@@ -18,13 +18,13 @@
         <h1>Experiences</h1>
         <div class="experiences">
           <div
-            class="experience"
             v-for="experience in experiencesList"
             :key="experience.id"
+            class="experience"
           >
             <Card>
               <template #header>
-                <i :class="experience.icon" style="font-size: 2.5rem"></i>
+                <i :class="experience.icon"></i>
               </template>
               <template #title>{{ experience.label }}</template>
               <template #content>
@@ -48,18 +48,16 @@
         <h1>Services</h1>
 
         <div class="services">
-          <div class="service" v-for="service in serviceList" :key="service.id">
-            <Card>
-              <template #title>{{ service.title }}</template>
+          <div v-for="service in serviceList" :key="service.id" class="service">
+            <Card :status="service.status">
+              <template #header>
+                <i :class="service.icon" :visible="service.icon"></i>
+              </template>
+              <template #title>{{ service.name }}</template>
               <template #content>
                 <p class="m-0">
                   {{ service.description }}
                 </p>
-              </template>
-              <template #footer>
-                <div class="flex gap-3 mt-1">
-                  <Button label="Open" class="w-full" />
-                </div>
               </template>
             </Card>
           </div>
@@ -72,7 +70,7 @@
             <Card>
               <template #title>Settings</template>
               <template #content>
-                <Button outlined class="border-2" @click="openSettings($event)">
+                <Button outlined @click="openSettings($event)">
                   <tune-icon v-tooltip="$t(`tooltips.settings`)" :size="50" />
                 </Button>
               </template>
@@ -85,7 +83,6 @@
 </template>
 
 <script>
-  import { isProxy, toRaw } from "vue"
   import { getModule } from "vuex-module-decorators"
   import MenuBar from "@/components/Menu/MenuBar.vue"
   import MainMenu from "@/components/Menu/MainMenu.vue"
@@ -93,8 +90,8 @@
   import Button from "primevue/button"
   import TuneIcon from "vue-material-design-icons/Tune.vue"
 
-  import Settings from "@/store/Modules/Settings"
-  const settingsModule = getModule(Settings)
+  import SettingsStore from "@/store/Modules/Settings"
+  const settingsModule = getModule(SettingsStore)
 
   import Services from "@/store/Modules/Services"
   const servicesModule = getModule(Services)
@@ -108,18 +105,33 @@
         services: [
           {
             id: 1,
-            title: "Service 1",
+            name: "Service 1",
             description: "Description 1",
+            type: "",
+            status: 1,
+            url: "",
+            icon: "",
+            enabled: true,
           },
           {
             id: 2,
-            title: "Service 2",
+            name: "Service 2",
             description: "Description 2",
+            type: "",
+            status: 120,
+            url: "",
+            icon: "",
+            enabled: true,
           },
           {
             id: 3,
-            title: "Service 3",
+            name: "Service 3",
             description: "Description 3",
+            type: "",
+            status: -1,
+            url: "",
+            icon: "",
+            enabled: true,
           },
         ],
         showMainOverlayMenu: false,
@@ -127,12 +139,30 @@
       }
     },
     computed: {
+      focusMode() {
+        return settingsModule.data.focus
+      },
       experiencesList() {
         return this.experiences.filter((item) => item.id !== "welcome")
       },
       serviceList() {
-        console.log("serviceList", servicesModule.serviceList)
-        return servicesModule.serviceList
+        return this.services
+        // return this.services.filter((item) => item.enabled == true)
+      },
+      serviceStatusColorList() {
+        console.log(
+          "serviceStatusColorList",
+          servicesModule.serviceStatusColorList
+        )
+        return servicesModule.serviceStatusColorList
+      },
+      serviceTypeList() {
+        console.log("serviceTypeList", servicesModule.serviceTypeList)
+        return servicesModule.serviceTypeList
+      },
+      serviceStatusList() {
+        console.log("serviceStatusList", servicesModule.serviceStatusList)
+        return servicesModule.serviceStatusList
       },
     },
     async mounted() {
@@ -140,7 +170,7 @@
       this.experiences = await this.getExperiences()
 
       // get all the services
-      this.services = await this.getServices()
+      // this.services = await this.getServices()
     },
     methods: {
       //Onload object tag
@@ -168,8 +198,9 @@
         console.log("open url", url)
         this.$router.push(url)
       },
-      handleClick(event) {
-        console.log(event)
+      openService(url) {
+        console.log("open service", url)
+        window.open(url, "_blank")
       },
     },
   }
@@ -177,4 +208,158 @@
 
 <style lang="scss" scoped>
   @import "./style.scss";
+
+  .service-status {
+    margin-left: auto;
+    margin-right: 1rem;
+    height: 1rem;
+    width: 1rem;
+    padding-left: 1rem;
+    background-color: gray;
+    border-radius: 50%;
+    display: inline-block;
+
+    //TODO: turn this into a computed property
+    &[status="-10"] {
+      background-color: v-bind("serviceStatusColorList['error']");
+    }
+    &[status="-1"] {
+      background-color: v-bind("serviceStatusColorList['error']");
+    }
+    &[status="0"] {
+      background-color: v-bind("serviceStatusColorList['disabled']");
+    }
+    &[status="1"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="10"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="11"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="14"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="15"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="20"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="25"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="30"] {
+      background-color: v-bind("serviceStatusColorList['installed']");
+    }
+    &[status="50"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="65"] {
+      background-color: v-bind("serviceStatusColorList['stopping']");
+    }
+    &[status="70"] {
+      background-color: v-bind("serviceStatusColorList['stopping']");
+    }
+    &[status="75"] {
+      background-color: v-bind("serviceStatusColorList['stopping']");
+    }
+    &[status="80"] {
+      background-color: v-bind("serviceStatusColorList['stopped']");
+    }
+    &[status="90"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="100"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="104"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="105"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="120"] {
+      background-color: v-bind("serviceStatusColorList['started']");
+    }
+    &[status="200"] {
+      background-color: v-bind("serviceStatusColorList['completedwerror']");
+    }
+    &[status="220"] {
+      background-color: v-bind("serviceStatusColorList['completed']");
+    }
+  }
+
+  .p-card {
+    &[status="-10"] {
+      background-color: v-bind("serviceStatusColorList['error']");
+    }
+    &[status="-1"] {
+      background-color: v-bind("serviceStatusColorList['error']");
+    }
+    &[status="0"] {
+      background-color: v-bind("serviceStatusColorList['disabled']");
+    }
+    &[status="1"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="10"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="11"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="14"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="15"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="20"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="25"] {
+      background-color: v-bind("serviceStatusColorList['installing']");
+    }
+    &[status="30"] {
+      background-color: v-bind("serviceStatusColorList['installed']");
+    }
+    &[status="50"] {
+      background-color: v-bind("serviceStatusColorList['available']");
+    }
+    &[status="65"] {
+      background-color: v-bind("serviceStatusColorList['stopping']");
+    }
+    &[status="70"] {
+      background-color: v-bind("serviceStatusColorList['stopping']");
+    }
+    &[status="75"] {
+      background-color: v-bind("serviceStatusColorList['stopping']");
+    }
+    &[status="80"] {
+      background-color: v-bind("serviceStatusColorList['stopped']");
+    }
+    &[status="90"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="100"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="104"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="105"] {
+      background-color: v-bind("serviceStatusColorList['starting']");
+    }
+    &[status="120"] {
+      background-color: v-bind("serviceStatusColorList['started']");
+    }
+    &[status="200"] {
+      background-color: v-bind("serviceStatusColorList['completedwerror']");
+    }
+    &[status="220"] {
+      background-color: v-bind("serviceStatusColorList['completed']");
+    }
+  }
 </style>
